036acfdd442bde2514743206ca6bc6bbc80941f5
mkomod <mkomodromos01@gmail.com>
2021-02-18 16:56:36 +0200

    up: test optimisation of kernel hyperparams

--- tests/test_kernels.R
+++ tests/test_kernels.R
@@ -1,7 +1,8 @@
 library(Rcpp)
-# Rcpp::sourceCpp("../src/gp.cpp", verbose=T, rebuild=T)
+
+Rcpp::sourceCpp("../src/gp.cpp", verbose=T, rebuild=T)
 Rcpp::sourceCpp("../src/optim.cpp", verbose=T, rebuild=T)
 
-# X <- matrix(1:100, ncol=1)
-# y <- sin(X / 20) + rnorm(nrow(X), 0, 0.2)
-# plot(X, y)
+X <- matrix(1:100, ncol=1)
+y <- sin(X / 20) + rnorm(nrow(X), 0, 0.2)
+gp_opt(y, X, 0.2, matrix(c(1,1), ncol=1))
