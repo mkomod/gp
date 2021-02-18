16eec3d3f40859190a63f7757ce205841acdc6c1
mkomod <mkomodromos01@gmail.com>
2021-02-18 14:19:49 +0200

    rf: minor changes, add: cg function header

--- src/optim.cpp
+++ src/optim.cpp
@@ -3,20 +3,21 @@
 */
 #include "optim.h"
 
-arma::colvec
-cg(arma::colvec x, optimfn fn, optimgr gr, double fn_min, double tol, int maxit,
-	int type, int trace) {
-    int n = x.n_elem;
 
-    arma::colvec x_in = arma::colvec(n, arma::fill::zeros);
+arma::colvec
+cg(arma::colvec x, optimfn fn, optimgr gr, double fn_min, double tol, 
+	int maxit, int type, bool verbose, void* ex) {
+
+    int n = x.n_elem;
     arma::colvec x_out = arma::colvec(n, arma::fill::zeros);
     int fail;		// failed
-    void *ex;		//
-    int fncount;	//
-    int grcount;	//
+    int fncount;	// number of times fn called
+    int grcount;	// number of times gr called
 
-    cgmin(n, x_in.memptr(), x_out.memptr(), &fn_min, fn, gr,
-	    &fail, tol, tol, ex, type, trace, &fncount, &grcount, maxit);
+    cgmin(n, x.memptr(), x_out.memptr(), &fn_min, fn, gr,
+	&fail, tol, tol, ex, type, (int) verbose, &fncount, 
+	&grcount, maxit);
 
     return x_out;
 }
+
--- src/optim.h
+++ src/optim.h
@@ -12,4 +12,9 @@ void cgmin(int n, double *Bvec, double *X, double *Fmin, optimfn fn, optimgr gr,
            int *fail, double abstol, double intol, void *ex, int type,
            int trace, int *fncount, int *grcount, int maxit);
 }
+
+arma::colvec
+cg(arma::colvec x, optimfn fn, optimgr gr, double fn_min, double tol, 
+	int maxit, int type, bool verbose, void* ex);
+
 #endif
